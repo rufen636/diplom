91690920ffa2fce7223895ac0f5daa05782da67c
rufen636 <rovik636@mail.ru>
2025-12-12 11:25:48 +0300

    new some migrations

--- database/migrations/2025_12_09_125234_create_services_table.php
+++ database/migrations/2025_12_09_125234_create_services_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->comment('Название услуги');
+            $table->string('code')->unique()->comment('Внутренний код услуги');
+            $table->text('description')->nullable()->comment('Описание услуги');
+            $table->decimal('price', 10, 2)->comment('Базовая цена');
+            $table->boolean('is_active')->default(true)->comment('Активна ли услуга');
+            $table->boolean('static_ip')->default(false)->comment('Статический IP');
+            $table->foreignId('service_category_id')->constrained('services_categories')->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('services')->onDelete('cascade')
+                ->comment('Родительская услуга для пакетов');
+            $table->decimal('internet_speed', 10, 2)->nullable()->comment('Скорость скачивания (Мбит/с)');
             $table->timestamps();
         });
     }
