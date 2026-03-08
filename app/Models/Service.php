0a9d369fe4fcf78813affd586444a748447be5f7
rufen636 <rovik636@mail.ru>
2026-03-08 22:15:39 +0300

    added some logic for manager

---
+++ app/Models/Service.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Service extends Model
+{
+    public function equipments()
+    {
+        return $this->belongsToMany(Equipment::class);
+    }
+}
